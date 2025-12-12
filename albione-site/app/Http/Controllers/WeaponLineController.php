d04572619ace2c57dd4c85443492f9cb8603a5ea
Nikita Stogovs <86836809+Xaxalka@users.noreply.github.com>
2025-12-12 16:20:37 +0200

    Embed branch summaries in weapon line tabs

--- albione-site/app/Http/Controllers/WeaponLineController.php
+++ albione-site/app/Http/Controllers/WeaponLineController.php
@@ -11,10 +11,16 @@ class WeaponLineController extends Controller
     {
         $lines = WeaponLine::whereIn('name', WeaponLine::ALLOWED_NAMES)
             ->with(['weapons' => function ($query) {
-            $query->orderBy('tier');
+            $query->orderBy('name');
         }])->orderBy('name')->get();
 
-        return view('weapon-lines.index', compact('lines'));
+        $quickBranches = [
+            'Warrior Weapons' => 'Арбалеты; Боевые перчатки; Молотки; Булавы; Топоры; Мечи',
+            'Hunter Weapons' => 'Луки; Кинжалы; Копья; Шесты; Shapershifts; Друиды',
+            'Mage Weapons' => 'Огненные посохи; Священные посохи; Мистические посохи; Морозные посохи; Проклятые посохи',
+        ];
+
+        return view('weapon-lines.index', compact('lines', 'quickBranches'));
     }
 
     public function show(string $slug)
