3260bbee2b20bb5a2bd02ca90bcc4c273145802b
Xaxalka <nikitastogov9@gmail.com>
2025-12-15 10:57:57 +0200

    ...

--- albione-site/resources/views/wiki.blade.php
+++ albione-site/resources/views/wiki.blade.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Cinzel:wght@600;700&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         :root {
             color-scheme: dark;
