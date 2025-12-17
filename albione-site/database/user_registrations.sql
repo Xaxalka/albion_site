130b2004daca56fdf6ce1f1f6479c06054a2eb10
Xaxalka <nikitastogov9@gmail.com>
2025-12-17 10:47:49 +0200

    new_user

--- albione-site/database/user_registrations.sql
+++ albione-site/database/user_registrations.sql
@@ -1,2 +1,3 @@
 -- User registration records (plain text passwords)
 
+INSERT INTO users (name, email, password, created_at, updated_at) VALUES ('Prime1', 'asddsfsdf@gmail.com', 'Prime228', '2025-12-17 08:46:29', '2025-12-17 08:46:29');
