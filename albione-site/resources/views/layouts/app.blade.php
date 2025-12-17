14acf295b29ed613633509435c6ba20dc99df4de
Nikita Stogovs <86836809+Xaxalka@users.noreply.github.com>
2025-12-17 11:15:26 +0200

    Add admin dashboard link for weapon line panel (#43)

--- albione-site/resources/views/layouts/app.blade.php
+++ albione-site/resources/views/layouts/app.blade.php
@@ -176,6 +176,7 @@
             <a href="{{ route('weapons.index') }}">Оружие</a>
             @auth
                 @if(Auth::user()?->is_admin)
+                    <a href="{{ route('admin.dashboard') }}">Админ-панель</a>
                     <a href="{{ route('admin.weapon-lines.index') }}">Ветки (админ)</a>
                     <a href="{{ route('admin.weapons.index') }}">Оружие (админ)</a>
                 @endif
